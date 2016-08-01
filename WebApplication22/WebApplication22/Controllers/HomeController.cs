729c3ed7956f9a9a953896082711ad90f50cf144
tshediso-tlaka <tlaka135@gmail.com>
2016-08-01 15:01:43 +0200

    Commit comment

---
+++ WebApplication22/WebApplication22/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+
+using System.Web.Mvc;
+
+namespace WebApplication22.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Homep()
+        {
+            return View();
+        }
+        public ActionResult Registration()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult BusinessEval()
+        {
+            return View();
+        }
+        public ActionResult  SetGoals()
+        {
+            return View();
+        }
+        public ActionResult Totalsummary()
+        {
+            return View();
+        }
+        public ActionResult GoalsVsDataSet()
+        {
+            return View();
+        }
+        public ActionResult home()
+        {
+            return View();
+        }
+        public ActionResult Resources()
+        {
+            return View();
+        }
+        public ActionResult DataManagement()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ WebApplication22/WebApplication22/scripts/JavaScript.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="angular.min.js" />
+
+var app = angular.module("myModule", [])
+    .controller("myController", function ($scope)
+    {
+            var lesley = {
+                Firstname : "lesley",
+                LastName : "Huma",
+                Gender : "Male"
+            }
+            $scope.leslee = lesley;
+    })
